74630fbbc914247a7577c5b949db0d9a1292e6b6
LuizEduardo-service <luizeduardo.service@outlook.com>
2024-06-17 21:13:55 -0300

    feat: go exercicios

---
+++ functions/function.go
@@ -0,0 +1,14 @@
+package functions
+
+import "fmt"
+
+func PrintArray(list []int) {
+
+	for _, value := range list {
+		fmt.Printf("%d\t", value)
+	}
+}
+
+func RemoverNumerosPares() {
+
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+
+	slice_ := []int{}
+	slice_ = append(slice_, 5)
+	slice_ = append(slice_, 4)
+	slice_ = append(slice_, 3)
+	PrintArray(slice_)
+}
