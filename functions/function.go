e66c5f9692cb845aefecc3c2cd9957d08c97af45
LuizEduardo-service <luizeduardo.service@outlook.com>
2024-06-18 07:54:55 -0300

    feat: continuação execicios

--- functions/function.go
+++ functions/function.go
@@ -3,12 +3,52 @@ package functions
 import "fmt"
 
 func PrintArray(list []int) {
-
+	fmt.Println("Imprimindo valores de um slice")
 	for _, value := range list {
 		fmt.Printf("%d\t", value)
 	}
 }
 
-func RemoverNumerosPares() {
+func RemoverNumerosPares(arr []int) (numerosPares []int) {
 
+	fmt.Println("removendo valores pares de um slice")
+	for _, value := range arr {
+
+		if value%2 != 0 {
+			numerosPares = append(numerosPares, value)
+		}
+
+	}
+	return
+}
+
+func InvertendoLista(arr []int) []int {
+
+	fmt.Println("invertendo lista")
+	start := 0
+	end := len(arr) - 1
+
+	for start < end {
+		arr[start], arr[end] = arr[end], arr[start]
+		start++
+		end--
+	}
+
+	return arr
+}
+
+func MovendoZerosParaOFinalDaLista(arr []int) []int {
+	fmt.Println("Movendo Zeros para o final da lista")
+	j := 0
+	for i := 0; i < len(arr); i++ {
+
+		if arr[i] != 0 && arr[j] == 0 {
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+		if arr[j] != 0 {
+			j++
+		}
+
+	}
+	return arr
 }
--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/LuizEduardo-service/go_nada/functions"
+)
+
 func main() {
 
 	slice_ := []int{}
 	slice_ = append(slice_, 5)
 	slice_ = append(slice_, 4)
+	slice_ = append(slice_, 0)
 	slice_ = append(slice_, 3)
-	PrintArray(slice_)
+	slice_ = append(slice_, 0)
+	slice_ = append(slice_, 10)
+
+	functions.PrintArray(slice_)
+	fmt.Println(functions.RemoverNumerosPares(slice_))
+	fmt.Println(functions.InvertendoLista(slice_))
+	fmt.Println(functions.MovendoZerosParaOFinalDaLista(slice_))
+
 }
